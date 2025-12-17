17f435480ef3af95fde441ca9152567b2d9f54a8
koby47 <samuelmensahquaye@gmail.com>
2025-12-17 21:48:14 +0000

    booking route optional auth

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -23,13 +23,6 @@ const router = express.Router();
 router.post(
   '/',
   optionalAuth,
-  (req, res, next) => {
-    // Pass auth state into Joi validation
-    req.validationContext = {
-      isGuest: !req.user
-    };
-    next();
-  },
   validate(createBookingSchema),
   createBooking
 );
